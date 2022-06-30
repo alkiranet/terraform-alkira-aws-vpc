60ba98993f1b094e6eee3cdb4b235b39dcedd8c2
wcollins <william.levi.collins@gmail.com>
2022-06-30 13:02:59 -0400

    Add example main.tf

---
+++ examples/onboard-complete-vpc/main.tf
@@ -0,0 +1,26 @@
+module "aws_vpc" {
+  source = "alkiranet/aws-vpc/alkira"
+
+  name    = "vpc-east"
+  cidr    = "10.1.0.0/16"
+
+  subnets = [
+    {
+      name = "app-subnet-a"
+      cidr = "10.1.1.0/24"
+      zone = "us-east-2a"
+    },
+    {
+      name = "app-subnet-b"
+      cidr = "10.1.2.0/24"
+      zone = "us-east-2b"
+    }
+  ]
+
+  cxp          = "US-EAST-2"
+  segment      = "corporate"
+  group        = "non-prod"
+  billing_tags = ["cloud", "network"]
+  credential   = "aws-auth"
+
+}
